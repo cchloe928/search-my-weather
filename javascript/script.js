5615ebfaf62c9a7b571866a0ef9cc91d81699867
chloegithub <miss_jihyun@hotmail.com>
2023-01-18 22:37:55 -0500

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -22,7 +22,7 @@ function displayWeather(city) {
                 .then(result => {
                     console.log(result)
 
-                    document.querySelector('#day0').textContent = result.list[5].dt_txt.split(' ')[0]
+                    document.querySelector('#day0').textContent = "Date: " + result.list[5].dt_txt.split(' ')[0]
                     document.querySelector('#icon').src = "http://openweathermap.org/img/wn/" + result.list[5].weather[0].icon + ".png"
                     document.querySelector('#temperature').textContent = result.list[5].main.temp + " F"
                     document.querySelector('#humidity').textContent = result.list[5].main.humidity + " %"
@@ -30,34 +30,34 @@ function displayWeather(city) {
                     document.querySelector('#uv-index').textContent = result
 
 
-                    document.querySelector('#day1').textContent = result.list[5].dt_txt.split(' ')[0]
+                    document.querySelector('#day1').textContent = "Date: " + result.list[5].dt_txt.split(' ')[0]
                     document.querySelector('#day1icon').src = "http://openweathermap.org/img/wn/" + result.list[5].weather[0].icon + ".png"
-                    document.querySelector('#day1temp').textContent = result.list[5].main.temp + " F"
-                    document.querySelector('#day1hum').textContent = result.list[5].main.humidity + " %"
+                    document.querySelector('#day1temp').textContent = "Temperature: " + result.list[5].main.temp + " F"
+                    document.querySelector('#day1hum').textContent = "Humidity: " + result.list[5].main.humidity + " %"
 
 
-                    document.querySelector('#day2').textContent = result.list[13].dt_txt.split(' ')[0]
+                    document.querySelector('#day2').textContent = "Date: " + result.list[13].dt_txt.split(' ')[0]
                     document.querySelector('#day2icon').src = "http://openweathermap.org/img/wn/" + result.list[13].weather[0].icon + ".png"
                     document.querySelector('#day2temp').textContent = "Temperature: " + result.list[13].main.temp + " F"
-                    document.querySelector('#day2hum').textContent = result.list[13].main.humidity + " %"
+                    document.querySelector('#day2hum').textContent = "Humidity: " + result.list[13].main.humidity + " %"
 
 
-                    document.querySelector('#day3').textContent = result.list[21].dt_txt.split(' ')[0]
+                    document.querySelector('#day3').textContent = "Date: " + result.list[21].dt_txt.split(' ')[0]
                     document.querySelector('#day3icon').src = "http://openweathermap.org/img/wn/" + result.list[21].weather[0].icon + ".png"
-                    document.querySelector('#day3temp').textContent = result.list[21].main.temp + " F"
-                    document.querySelector('#day3hum').textContent = result.list[21].main.humidity + " %"
+                    document.querySelector('#day3temp').textContent = "Temperature: " + result.list[21].main.temp + " F"
+                    document.querySelector('#day3hum').textContent = "Humidity: " + result.list[21].main.humidity + " %"
 
 
-                    document.querySelector('#day4').textContent = result.list[29].dt_txt.split(' ')[0]
+                    document.querySelector('#day4').textContent = "Date: " + result.list[29].dt_txt.split(' ')[0]
                     document.querySelector('#day4icon').src = "http://openweathermap.org/img/wn/" + result.list[29].weather[0].icon + ".png"
-                    document.querySelector('#day4temp').textContent = result.list[19].main.temp + " F"
-                    document.querySelector('#day4hum').textContent = result.list[19].main.humidity + " %"
+                    document.querySelector('#day4temp').textContent = "Temperature: " + result.list[19].main.temp + " F"
+                    document.querySelector('#day4hum').textContent = "Humidity: " + result.list[19].main.humidity + " %"
 
 
-                    document.querySelector('#day5').textContent = result.list[37].dt_txt.split(' ')[0]
+                    document.querySelector('#day5').textContent = "Date: " + result.list[37].dt_txt.split(' ')[0]
                     document.querySelector('#day5icon').src = "http://openweathermap.org/img/wn/" + result.list[37].weather[0].icon + ".png"
-                    document.querySelector('#day5temp').textContent = result.list[37].main.temp + " F"
-                    document.querySelector('#day5hum').textContent = result.list[37].main.humidity + " %"
+                    document.querySelector('#day5temp').textContent = "Temperature: " + result.list[37].main.temp + " F"
+                    document.querySelector('#day5hum').textContent = "Humidity: " + result.list[37].main.humidity + " %"
 
                     console.log(result.list[5].dt_txt.split(' ')[0])
                 })
